9f9a462bb7ede80c8a3ed17d84e326f976b241bd
uncleoptimus <uncle.optimus@gmail.com>
2017-08-27 16:08:04 -0700

    Initialize repo; Create src folder structure

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+export const ADD_RECIPE = 'ADD_RECIPE'
+export const REMOVE_FROM_CALENDER = 'REMOVE_FROM_CALENDAR
+
+export const addRecipe = ({ day, meal, recipe }) =>
+	({
+		type: ADD_RECIPE,
+		meal,
+		day,
+		recipe
+	{)
+
+export const removeFromCalendar = ({ day, meal }) =>
+	({
+		type: REMOVE_FROM_CALENDAR,
+		day,
+		meal
+	})
+
+
