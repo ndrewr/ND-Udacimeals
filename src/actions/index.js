a5bef5780b89464d91027518974741f883370497
uncleoptimus <uncle.optimus@gmail.com>
2017-08-30 16:36:10 -0700

    Hook up basic view to store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,23 @@
 
-export const ADD_RECIPE = 'ADD_RECIPE'
-export const REMOVE_FROM_CALENDER = 'REMOVE_FROM_CALENDAR'
+export const ADD_RECIPE = 'ADD_RECIPE';
 
-export const addRecipe = ({ day, meal, recipe }) =>
-	({
+export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
+
+export function addRecipe ({ day, meal, recipe }) {
+	return {
 		type: ADD_RECIPE,
 		meal,
 		day,
 		recipe
-	{)
+	}
+}
 
-export const removeFromCalendar = ({ day, meal }) =>
-	({
-		type: REMOVE_FROM_CALENDAR,
+export function removeFromCalendar ({ day, meal }) {
+	return {
+		type: 'REMOVE_FROM_CALENDAR',
 		day,
 		meal
-	})
+	}
+}
 
 
